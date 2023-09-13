fbf132fe4fd6eeeda23a0eb3f7244045647dceb1
LeonelBrav0 <mfleonelb@gmail.com>
2023-09-13 02:20:08 -0700

    Added wave dump into new simulation script

--- hardware/sim/sim.tcl
+++ hardware/sim/sim.tcl
@@ -10,16 +10,28 @@ set RTL_FILELIST "../filelist/rtl.f"
 
 set VSIM_OPTIONS "work.${TOP_SIM_MODULE} "
 
+set WAVE_LIST "/*"
+
+# Add -wlf option to generate waveform file
+if {$DUMP_WAVEFORM == 1} {
+    set VSIM_OPTIONS "$VSIM_OPTIONS -wlf "
+}
+
 #set VLOG_OPTIONS " -32 -lint -sv -f ${RTL_FILELIST} tb/${TOP_SIM_MODULE}.sv -top ${TOP_SIM_MODULE}"
 set VLOG_OPTIONS "tb/${TOP_SIM_MODULE}.sv -f ${RTL_FILELIST}"
 
 if {$GTEST_DPI == 1} {
-    set VSIM_OPTIONS "$VSIM_OPTIONS -sv_root ${GTEST_DPI_PATH} -sv_lib ${GTEST_DPI_LIB}"
+    set VSIM_OPTIONS "$VSIM_OPTIONS -sv_root ${GTEST_DPI_PATH} -sv_lib ${GTEST_DPI_LIB} add wave *"
 }
 
 eval [format "vlog %s" $VLOG_OPTIONS]
 eval [format "vsim %s" $VSIM_OPTIONS]
+eval [format "add wave -r $WAVE_LIST"]
 eval [format "run -all"]
+if {$DUMP_WAVEFORM == 1} {
+    eval [format "wave export -file vsim.vcd -format vcd -designunit $TOP_SIM_MODULE -f"]
+}
+eval [format "quit"]
 
 #eval [format "qrun %s %s %s" $VLOG_OPTIONS $VOPT_OPTIONS $VSIM_OPTIONS]
 
--- hardware/sim/tb/q_top_tb.sv
+++ hardware/sim/tb/q_top_tb.sv
@@ -1,14 +1,76 @@
+`include "project_parameters.vh"
+
 module q_top_tb();
 
 
 reg  [15:0] a;
 reg  [15:0] b;
-wire [15:0] c;
+reg  [15:0] c;
+
+
+//-------------------------IO DECLARATION-------------------------//
+// Main 
+reg                                         aresetn             ;
+reg                                         aclk                ;
+// To SRAM
+reg  [   `HW_BLK_MEM_GEN_0_AWIDTH - 1 : 0]  addr                ;  
+wire                                        clk                 ;
+reg  [   `HW_BLK_MEM_GEN_0_DWIDTH - 1 : 0]  din                 ;
+wire [   `HW_BLK_MEM_GEN_0_DWIDTH - 1 : 0]  dout                ;  
+reg                                         en                  ;
+reg                                         rst                 ;
+reg  [ `HW_BLK_MEM_GEN_0_WE_WIDTH - 1 : 0]  we                  ;
+// To ov7670
+wire PCLK                                                       ;
+wire HREF                                                       ;
+wire VSYNC                                                      ;
+wire [          `HW_OV7670_DWIDTH - 1 : 0]  D                   ;
+reg  RST                                                        ;
+wire XCLK                                                       ;
+
+initial begin
+    aclk = 0;
+    forever begin
+        aclk = ~aclk; #10;
+    end
+
+end
 
 initial begin
     $display("HELLO WORLD TESTBENCH");
+    a = 0;
+    b = 0;
+    c = 0;
+    $display("a=%d, b=%d, c=%d", a, b, c);
+    #10;
+    a = 1;
+    b = 1;
+    c = 1;
+    $display("a=%d, b=%d, c=%d", a, b, c);
+    #10;
+    a = 2;
+    b = 2;
+    c = 2;
+    $display("a=%d, b=%d, c=%d", a, b, c);
+    $stop;
 end
 
-
+hw_ov7670_src u_hw_ov7670_src(
+.aresetn(aresetn),
+.aclk   (aclk   ),
+.addr   (addr   ),
+.clk    (clk    ),
+.din    (din    ),
+.dout   (dout   ),
+.en     (en     ),
+.rst    (rst    ),
+.we     (we     ),
+.PCLK   (PCLK   ),                                       
+.HREF   (HREF   ),                                       
+.VSYNC  (VSYNC  ),                                       
+.D      (D      ),
+.RST    (RST    ),                                       
+.XCLK   (XCLK   )                                       
+);
 
 endmodule
